98e9d9a55e1e4bec7d180ff14653b5a3ce49a826
Shardindu <75495716+Shardindu05@users.noreply.github.com>
2022-07-21 15:24:59 +0530

    Add files via upload

---
+++ demobyshardindu/src/main/java/com/example/shardindu/demobyshardindu/DemobyshardinduApplication.java
@@ -0,0 +1,13 @@
+package com.example.shardindu.demobyshardindu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemobyshardinduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemobyshardinduApplication.class, args);
+	}
+
+}
---
+++ demobyshardindu/src/test/java/com/example/shardindu/demobyshardindu/DemobyshardinduApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shardindu.demobyshardindu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemobyshardinduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
